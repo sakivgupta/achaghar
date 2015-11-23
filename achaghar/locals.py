8c446a19e7aa76055125c7c3df4b55d1a9a8466c
shriyanka <shriyanka26@gmail.com>
2015-11-23 22:25:48 +0530

    first commit

---
+++ achaghar/locals.py
@@ -0,0 +1,10 @@
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ghar',
+            'USER': 'ghar123',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '',
+        }
+    }
